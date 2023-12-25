bc07bb7143e75e923ad29885c4993407901e4428
Ventsislav Nikolov <ventsislavnikolovv@gmail.com>
2023-12-25 10:46:18 +0200

    chore(update): update packages and add tailwindcss eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,46 @@
-import antfu from '@antfu/eslint-config'
+import { antfu, combine } from '@antfu/eslint-config';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default antfu({
-  react: true,
-  typescript: true,
-  formatters: {
-    css: true,
-    html: true,
-    markdown: 'prettier',
-  },
-  ignores: [
-    './fixtures',
-    '/node_modules',
-    '/.github',
-    '/.vscode',
-  ],
-  rules: {
-    'curly': 'off',
-    'style/semi': 'off',
-    'no-console': 'warn',
-    'no-unused-vars': 'warn',
-    'style/jsx-indent': 'off',
-    'react/display-name': 'off',
-    'jsdoc/check-param-names': 'off',
-    'node/prefer-global/process': 'off',
-    'unused-imports/no-unused-vars': 'warn',
-    'jsdoc/require-property-description': 'off',
-    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-  },
-  stylistic: {
-    indent: 2,
-    quotes: 'single',
-  },
-})
+const compat = new FlatCompat()
+
+const customConfig = [
+  ...compat.config({
+    extends: ['plugin:tailwindcss/recommended'],
+  }),
+]
+
+export default combine(
+  antfu({
+    react: true,
+    typescript: true,
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+    ignores: [
+      './fixtures',
+      '/node_modules',
+      '/.github',
+      '/.vscode',
+    ],
+    rules: {
+      'curly': 'off',
+      'style/semi': 'off',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'style/jsx-indent': 'off',
+      'react/display-name': 'off',
+      'jsdoc/check-param-names': 'off',
+      'node/prefer-global/process': 'off',
+      'unused-imports/no-unused-vars': 'warn',
+      'jsdoc/require-property-description': 'off',
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    },
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+    },
+  }),
+  customConfig,
+)
